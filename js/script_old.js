fc0b5b232f59706666bbf117a941133880999335
Janis <48187827+JaN1cee@users.noreply.github.com>
2021-01-02 14:15:00 +0100

    Create script_old.js

---
+++ js/script_old.js
@@ -0,0 +1,117 @@
+var score = 100000;
+var scorePerSecond = 0;
+var clickAmount = 1;
+
+var farmer = 0;
+var swordsmen = 0;
+
+var farmerCost = 10;
+var swordsmenCost = 100;
+
+function addToScore(clickAmount) {
+  score = score + clickAmount;
+  document.getElementById("score").innerHTML = score;
+}
+
+function addFarmer() {
+  if (score >= farmerCost) {
+    score = score - farmerCost;
+    farmer = farmer + 1;
+    farmerCost = Math.round(farmerCost * 1.15);
+
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("farmer").innerHTML = farmer;
+    document.getElementById("farmerCost").innerHTML = farmerCost;
+    updateScorePerSecond();
+  }
+}
+
+function addSwordsmen() {
+  if (score >= swordsmenCost) {
+    score = score - swordsmenCost;
+    swordsmen = swordsmen + 1;
+    swordsmenCost = Math.round(swordsmenCost * 1.15);
+
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("swordsmen").innerHTML = swordsmen;
+    document.getElementById("swordsmenCost").innerHTML = swordsmenCost;
+    updateScorePerSecond();
+  }
+}
+
+function updateScorePerSecond() {
+  scorePerSecond = farmer * 0.5 + swordsmen * 2;
+  document.getElementById("scorePerSecond").innerHTML = scorePerSecond;
+}
+
+setInterval(function () {
+  score = score + farmer * 0.5 + swordsmen * 2;
+  document.getElementById("score").innerHTML = score;
+}, 1000);
+
+setInterval(function () {
+  saveGame();
+}, 30000);
+
+document.addEventListener(
+  "keydown",
+  function (event) {
+    if (event.ctrlKey && event.which == 83) {
+      event.preventDefault();
+      saveGame();
+    }
+  },
+  false
+);
+
+function saveGame() {
+  var gameSave = {
+    score: score,
+    clickAmount: clickAmount,
+
+    farmer: farmer,
+    farmerCost: farmerCost,
+
+    swordsmen: swordsmen,
+    swordsmenCost: swordsmenCost,
+  };
+  localStorage.setItem("gameSave", JSON.stringify(gameSave));
+}
+
+function loadGame() {
+  var savedGame = JSON.parse(localStorage.getItem("gameSave"));
+  if (typeof savedGame.score !== "undefined") score = savedGame.score;
+  if (typeof savedGame.clickAmount !== "undefined")
+    clickAmount = savedGame.clickAmount;
+
+  if (typeof savedGame.farmer !== "undefined") farmer = savedGame.farmer;
+  if (typeof savedGame.farmerCost !== "undefined")
+    farmerCost = savedGame.farmerCost;
+
+  if (typeof savedGame.swordsmen !== "undefined")
+    swordsmen = savedGame.swordsmen;
+  if (typeof savedGame.swordsmenCost !== "undefined")
+    swordsmenCost = savedGame.swordsmenCost;
+}
+
+function resetGame() {
+  if (confirm("Are you sure you want to reset your game?")) {
+    var gameSave = {};
+    localStorage.setItem("gameSave", JSON.stringify(gameSave));
+    location.reload();
+  }
+}
+
+window.onload = function () {
+  loadGame();
+  updateScorePerSecond();
+  document.getElementById("scorePerSecond").innerHTML = scorePerSecond;
+
+  document.getElementById("score").innerHTML = score;
+  document.getElementById("swordsmen").innerHTML = swordsmen;
+  document.getElementById("swordsmenCost").innerHTML = swordsmenCost;
+
+  document.getElementById("score").innerHTML = score;
+  document.getElementById("farmer").innerHTML = farmer;
+  document.getElementById("farmerCost").innerHTML = farmerCost;
+};
